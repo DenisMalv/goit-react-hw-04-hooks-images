92c065b5d0056295f0a1f560d043e3540c879061
DenisMalv <kissed03@gmail.com>
2022-11-14 01:15:33 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef} from 'react';
+import { useState, useEffect,} from 'react';
 import { MainContainer } from './App.styled.js';
 
 import Searchbar from './Searchbar/Searchbar';
